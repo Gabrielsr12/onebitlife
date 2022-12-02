d5ac40d71340b58b74cd7451e980f64ec623ee76
Gabrielsr12 <gabrielramos1231@gmail.com>
2022-12-01 21:02:59 -0300

    Usando o serviço SET no db

--- src/pages/AppExplanation/index.jsx
+++ src/pages/AppExplanation/index.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import DefaultButton from "../../Components/Common/DefaultButton";
 import ExplanationCard from "../../Components/Explanation/ExplanationCard";
+import ChangeNavigationService from "../../service/ChangeNavigationService";
 
 
 
 export default function AppExplanation() {
 
   const navigation = useNavigation();
+  const [showHome, setShowHome] = useState("false");
+  const startDate = new Date();
+  const appStartData = `${startDate.getFullYear()}-${startDate.getMonth()}-${startDate.getDate()}`;
 
   function handleNavHome() {
     navigation.navigate("Home");
+  }
+  function handleSetShowHome() {
+    if (showHome !== "true") {
+      ChangeNavigationService.setShowHome({ showHome: "true", appStartData })
+        .then(() => console.log(`Sucesso! ${showHome} ${appStartData}`))
+        .catch((err) => console.log(err));
+      setShowHome("true");
+
+      handleNavHome();
+    }
+
+    handleNavHome();
   }
 
   return (
@@ -31,7 +47,7 @@ export default function AppExplanation() {
           </Text>
           <DefaultButton
             buttonText={"Continuar"}
-            handlePress={handleNavHome}
+            handlePress={handleSetShowHome}
             width={250}
             height={50}
           />
